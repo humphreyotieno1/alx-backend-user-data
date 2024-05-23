a8f51fec7366152a0a0babaa744c4a177d899b58
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-23 20:48:42 +0300

    basic user object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,9 @@
 from api.v1.auth.auth import Auth
 import base64
 import binascii
+from typing import TypeVar
+from models.base import Base as b
+from models.user import User as u
 
 
 class BasicAuth(Auth):
