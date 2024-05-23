2dfbf1cb371958c72030f90cd134943ae72af6b5
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-23 20:25:03 +0300

    Decode base64

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,3 +14,14 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic'):
             return None
         return authorization_header[len('Basic'):]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """Decode base 64"""
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            return base64_authorization_header.encode('utf-8').decode('base64')
+        except Exception:
+            return None
