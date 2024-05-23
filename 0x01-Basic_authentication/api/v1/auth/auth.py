3f7f7595ae0076cdfd590bfd9472966e5f3e05c9
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-23 21:01:30 +0300

    Modify

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,13 +8,19 @@ class Auth:
     """manage api authentication"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Returns True if authentication is required for the given path."""
-        check = path
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
+
         if path[-1] != "/":
-            check += "/"
-        if check in excluded_paths or path in excluded_paths:
-            return False
+            path += "/"
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith("*"):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path or path.startswith(excluded_path + "/"):
+                return False
+
         return True
 
     def authorization_header(self, request=None) -> None:
