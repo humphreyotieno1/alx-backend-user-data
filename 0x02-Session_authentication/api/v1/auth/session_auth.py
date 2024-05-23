1fcfb96fd301a3e904bd6606cc7229899eef7763
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-23 22:53:04 +0300

    New view session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,10 @@
 from api.v1.auth.auth import Auth
 import uuid
 from models.user import User
+from flask import Flask, jsonify, request
+from api.v1.views import app_views
+from models.user import User
+from api.v1.app import auth
 
 
 class SessionAuth(Auth):
@@ -32,3 +36,28 @@ class SessionAuth(Auth):
         if user_id is None:
             return None
         return User.get(user_id)
+
+    @app_views.route('/auth_session/login',
+                     methods=['POST'], strict_slashes=False)
+    def session_login():
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if not email:
+            return jsonify({"error": "email missing"}), 400
+        if not password:
+            return jsonify({"error": "password missing"}), 400
+
+        user = User.search({'email': email})
+        if not user:
+            return jsonify({"error": "no user found for this email"}), 404
+
+        if not user[0].is_valid_password(password):
+            return jsonify({"error": "wrong password"}), 401
+
+        session_id = auth.create_session(user[0].id)
+        user_dict = user[0].to_json()
+        response = jsonify(user_dict)
+        response.set_cookie(auth.session_name, session_id)
+
+        return response
