0f9e4a3cd7f4f5f5c13a6e568fada000c78e5c15
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-31 00:28:24 +0300

    Get user profile

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -11,14 +11,8 @@ class User(Base):
     '''User model'''
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
-
-    def __repr__(self):
-        return "<User(id='%s', email='%s', hashed_password='%s', " \
-               "session_id='%s', reset_token='%s')>" % (
-                    self.id, self.email, self.hashed_password,
-                    self.session_id, self.reset_token)
