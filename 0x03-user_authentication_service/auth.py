61ca59df04cf477bef7af27921c8a90fab3479fc
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-31 00:01:30 +0300

    Validate user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,3 +30,15 @@ class Auth:
             raise ValueError('User {} already exists'.format(email))
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """validates user"""
+        try:
+            login_user = self._db.find_user_by(email=email)
+            if bcrypt.checkpw(password.encode('utf-8'),
+                              login_user.hashed_password):
+                return True
+            return False
+
+        except NoResultFound:
+            return False
