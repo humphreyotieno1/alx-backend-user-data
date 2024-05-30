f01d209a070fce16ba0fd33e3b3a328a86e80e99
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-30 23:23:43 +0300

    Hash a new password

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,8 @@
+import bcrypt
+
+
+def _hash_password(password: str) -> bytes:
+    """Hash a password"""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed_password
