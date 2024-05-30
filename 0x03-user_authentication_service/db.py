b0fd6ae2dec81005fac895190f0b40e21bdc4d66
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-30 23:07:31 +0300

    add new user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,11 +35,15 @@ class DB:
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """Adding a new user"""
-        user = User(email=email, hashed_password=hashed_password)
         session = self._session
-        session.add(user)
-        session.commit()
-        return user
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            session.add(new_user)
+            session.commit()
+        except Exception:
+            session.rollback()
+            new_user = None
+        return new_user
 
     def find_user_by(self, **kwargs) -> User:
         """Find user by email"""
